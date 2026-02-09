2b4ee9652b3c1be116bf389752f6c27bfb652e0f
RoelSabino <roel.sabino.050467@gmail.com>
2026-02-09 11:00:14 +0800

    edited prepared statement removing natural join

--- sql_PreparedStatement.sql
+++ sql_PreparedStatement.sql
@@ -1,1 +1,11 @@
-PREPARE GetOrderDetail FROM 'SELECT customers.FullName, orders.OrderID, menus.MenuName, menus.MenuDescription, orders.Quantity, menus.MenuPrice, orders.TotalCost FROM customers NATURAL JOIN orders NATURAL JOIN menus WHERE CustomerID = ? ';
+PREPARE GetOrderDetail FROM '
+    SELECT c.FullName, o.OrderID, m.MenuName, m.MenuDescription,
+           o.Quantity, m.MenuPrice, o.TotalCost
+    FROM customers c
+    JOIN orders o ON c.CustomerID = o.CustomerID
+    JOIN menus m ON o.MenuID = m.MenuID
+    WHERE c.CustomerID = ?
+';
+
+SET @id = 1;
+EXECUTE GetOrderDetail USING @id;
